ccf4de9441ad9e7929c0be473c4967c7e8393e8e
a_frear <amy.frear@gmail.com>
2021-02-15 13:29:25 -0500

    Update problems.js

--- problems.js
+++ problems.js
@@ -192,19 +192,29 @@ For example, the 7th Fibonacci number in a Fibonacci sequence is 13.
 The sequence looks as follows: 1, 1, 2, 3, 5, 8, 13.
 */
 
+// const fibonacci = function(n) {
+//     if (n ===1){
+//         return [0, 1]
+//     }
+//     else
+//     {
+//         const s = fibonacci(n-1)
+//         s.push(s[s.length - 1] + s[s.length - 2])
+//         return s
+//     }
+// }
+
+// console.log(fibonacci(13))
+
 const fibonacci = function(n) {
-    if (n ===1){
-        return [0, 1]
+    if (n === 1 || n === 2) {
+        return 1
     }
-    else
-    {
-        const s = fibonacci(n-1)
-        s.push(s[s.length - 1] + s[s.length - 2])
-        return s
-    }
+    return fibonacci(n-1) + fibonacci(n-2)
 }
 
-console.log(fibonacci(13))
+console.log(fibonacci(7))
+
 
 /*
 7. Factorial
@@ -245,3 +255,27 @@ function convertToBinary(num){
 
 console.log(convertToBinary(33))
 
+/*
+8. Find a way out of the maze
+You have entered a Maze and need to find your way out of it. There are more than one possible paths through the Maze to the single exit point. Write a recursive function that will help you find a possible path through the maze.
+
+You can use the following mazes to test your program.
+
+let mySmallMaze = [
+    [' ', ' ', ' '],
+    [' ', '*', ' '],
+    [' ', ' ', 'e']
+];
+
+let maze = [
+    [' ', ' ', ' ', '*', ' ', ' ', ' '],
+    ['*', '*', ' ', '*', ' ', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', '*', '*', '*', '*', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+];
+The Maze is represented as a N*M matrix (in the above case, a 3X3 or a 5X7 array). The starting point is the top left corner and the exit is indicated by e. For simplicity purpose, use the bottom right corner of the maze as the exit. You can't go outside the boundaries of the maze. The maze has passages that are blocked and you can't go through them. These blocked passages are indicated by *. Passing through a blocked cell as well as passing though a cell that you have already passed before are forbidden.
+
+For the above maze, a possible exit path can be RRDDLLDDRRRRRR
+
+*/
