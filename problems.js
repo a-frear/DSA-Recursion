aa155ab42bff97154f656dd1da3a9a0df1c2bbec
a_frear <amy.frear@gmail.com>
2021-02-16 17:07:54 -0500

    Update problems.js

--- problems.js
+++ problems.js
@@ -257,7 +257,9 @@ console.log(convertToBinary(33))
 
 /*
 8. Find a way out of the maze
-You have entered a Maze and need to find your way out of it. There are more than one possible paths through the Maze to the single exit point. Write a recursive function that will help you find a possible path through the maze.
+You have entered a Maze and need to find your way out of it. 
+There are more than one possible paths through the Maze to the single exit point. 
+Write a recursive function that will help you find a possible path through the maze.
 
 You can use the following mazes to test your program.
 
@@ -274,8 +276,70 @@ let maze = [
     [' ', '*', '*', '*', '*', '*', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', 'e']
 ];
-The Maze is represented as a N*M matrix (in the above case, a 3X3 or a 5X7 array). The starting point is the top left corner and the exit is indicated by e. For simplicity purpose, use the bottom right corner of the maze as the exit. You can't go outside the boundaries of the maze. The maze has passages that are blocked and you can't go through them. These blocked passages are indicated by *. Passing through a blocked cell as well as passing though a cell that you have already passed before are forbidden.
+The Maze is represented as a N*M matrix (in the above case, a 3X3 or a 5X7 array). 
+The starting point is the top left corner and the exit is indicated by e. 
+For simplicity purpose, use the bottom right corner of the maze as the exit. 
+You can't go outside the boundaries of the maze. The maze has passages that are blocked and you can't go through them. 
+These blocked passages are indicated by *. 
+Passing through a blocked cell as well as passing though a cell that you have already passed before are forbidden.
 
 For the above maze, a possible exit path can be RRDDLLDDRRRRRR
 
 */
+
+let mySmallMaze = [
+    [' ', ' ', ' '],
+    [' ', '*', ' '],
+    [' ', ' ', 'e']
+];
+
+let maze = [
+    [' ', ' ', ' ', '*', ' ', ' ', ' '],
+    ['*', '*', ' ', '*', ' ', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', '*', '*', '*', '*', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+]; 
+
+
+  const solveMaze = function(row, column, directions, maze){
+
+   if (maze[row][column] == 'e'){
+    return directions
+  }
+  
+ 
+  if (column < maze[0].length - 1 && maze[row][column + 1] != ('*')){
+    maze[row][column] = '*'
+    let newColumn = column + 1
+    let newRow = row
+    return directions = 'R' + solveMaze(newRow, newColumn, directions, maze)
+
+  }  
+    
+  if (row < maze.length - 1 && maze[row  + 1][column] != ('*')){
+    maze[row][column] = '*'
+    let newColumn = column
+    let newRow = row + 1
+    return directions = 'D' + solveMaze(newRow, newColumn, directions, maze)
+  }  
+    
+ if (column > 0 && maze[row][column - 1] != ('*')){
+    maze[row][column] = '*'
+    let newColumn = column - 1
+    let newRow = row
+    return directions = 'L' + solveMaze(newRow, newColumn, directions, maze)
+
+  }
+    
+   if (row > 0 && maze[row - 1][column] != ('*')){
+    maze[row][column] = '*'
+    let newColumn = column
+    let newRow = row - 1
+    return directions = 'U' + solveMaze(newRow, newColumn, directions, maze)
+  }    
+}
+
+console.log(solveMaze(0, 0, '', mySmallMaze))
+console.log(solveMaze(0, 0, '', maze))
+
